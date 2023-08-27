2be60926ac1cfb6ce89fcfa7d4f1a6c4f68ed46e
piomin <piotr.minkowski@gmail.com>
2023-08-27 23:45:59 +0200

    add several grpc services

--- account-service-grpc/src/main/java/pl/piomin/services/grpc/account/repository/AccountRepository.java
+++ account-service-grpc/src/main/java/pl/piomin/services/grpc/account/repository/AccountRepository.java
@@ -4,13 +4,17 @@ package pl.piomin.services.grpc.account.repository;
 import pl.piomin.services.grpc.account.model.AccountProto;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AccountRepository {
 
     List<AccountProto.Account> accounts;
+    AtomicInteger id;
 
     public AccountRepository(List<AccountProto.Account> accounts) {
         this.accounts = accounts;
+        this.id = new AtomicInteger();
+        this.id.set(accounts.size());
     }
 
     public List<AccountProto.Account> findAll() {
@@ -26,6 +30,15 @@ public class AccountRepository {
                 .filter(it -> it.getNumber().equals(number))
                 .findFirst()
                 .orElseThrow();
+    }
+
+    public AccountProto.Account add(int customerId, String number) {
+        AccountProto.Account a = AccountProto.Account.newBuilder()
+                .setId(id.incrementAndGet())
+                .setCustomerId(customerId)
+                .setNumber(number)
+                .build();
+        return a;
     }
 
 }
--- account-service-grpc/src/main/java/pl/piomin/services/grpc/account/service/AccountsService.java
+++ account-service-grpc/src/main/java/pl/piomin/services/grpc/account/service/AccountsService.java
@@ -7,6 +7,8 @@ import pl.piomin.services.grpc.account.model.AccountProto;
 import pl.piomin.services.grpc.account.model.AccountsServiceGrpc;
 import pl.piomin.services.grpc.account.repository.AccountRepository;
 
+import java.util.List;
+
 @GrpcService
 public class AccountsService extends AccountsServiceGrpc.AccountsServiceImplBase {
 
@@ -16,6 +18,29 @@ public class AccountsService extends AccountsServiceGrpc.AccountsServiceImplBase
     @Override
     public void findByNumber(AccountProto.FindByNumberRequest request, StreamObserver<AccountProto.Account> responseObserver) {
         AccountProto.Account a = repository.findByNumber(request.getNumber());
+        responseObserver.onNext(a);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void findByCustomer(AccountProto.FindByCustomerRequest request, StreamObserver<AccountProto.Accounts> responseObserver) {
+        List<AccountProto.Account> accounts = repository.findByCustomer(request.getCustomerId());
+        AccountProto.Accounts a = AccountProto.Accounts.newBuilder().addAllAccount(accounts).build();
+        responseObserver.onNext(a);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void findAll(AccountProto.FindAllRequest request, StreamObserver<AccountProto.Accounts> responseObserver) {
+        List<AccountProto.Account> accounts = repository.findAll();
+        AccountProto.Accounts a = AccountProto.Accounts.newBuilder().addAllAccount(accounts).build();
+        responseObserver.onNext(a);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void addAccount(AccountProto.Account request, StreamObserver<AccountProto.Account> responseObserver) {
+        AccountProto.Account a = repository.add(request.getCustomerId(), request.getNumber());
         responseObserver.onNext(a);
         responseObserver.onCompleted();
     }
